15a6749b7073f91bb7cfd1eb218cbe3f8c77a9cf
Piotr Szołtys <piotr.szoltys@kotrak.com>
2019-03-12 22:32:45 +0100

    check if shooted

--- ConsoleApp4/ConsoleApp4/Ship/Ship.cs
+++ ConsoleApp4/ConsoleApp4/Ship/Ship.cs
@@ -4,41 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Console.Point;
+using ConsoleApp4.Utilities;
 
 namespace ConsoleApp4.Ship
 {
     class Ship
     {
-       private int lifes { get; set; }
-       public Point Start { get; set; }
-       public Point End { get; set; }
+       private int Lifes { get; set; }
+       private Point Start { get; set; }
+       private Point End { get; set; }
 
-       
+        public bool isAlive {
+            get
+            {
+                return Lifes > 0;
+            }
+        }       
 
        public Ship(Point start, Point end) {
             Start = start;
             End = end;
             if (start.XPos == end.XPos)
             {
-                lifes = Math.Abs(end.YPos - start.YPos) + 1;
+                Lifes = Math.Abs(end.YPos - start.YPos) + 1;
             }
             else
-                lifes = Math.Abs(end.XPos - start.XPos) + 1; 
-        }
-        public void ReduceLife()
-        {
-            lifes--;
+                Lifes = Math.Abs(end.XPos - start.XPos) + 1; 
         }
 
-        public bool IsAlive()
-        {
-            return lifes > 0; 
-        }
+        private int ReduceLife() => Lifes--;
 
-        public void CheckIfShooted(Point xy)
+        public bool CheckIfShooted(Point shoot)
         {
-            
+            if (!shoot.CheckIfInRange(Start, End))
+                return false;
+            else if (shoot.YPos == Utils.LinearFunctionValue(shoot.XPos, Start, End))
+            {
+                ReduceLife();
+                return true;
+            }
+            return false;
         }
-
     }
 }
